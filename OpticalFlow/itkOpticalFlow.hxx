b86e01eb7349eb982e79b79a45dab47fc9f58963
isnardo <isnardo.rr@gmail.com>
2013-10-24 13:57:07 -0500

    little modification of hxx

--- OpticalFlow/itkOpticalFlow.hxx
+++ OpticalFlow/itkOpticalFlow.hxx
@@ -18,13 +18,27 @@ namespace itk
 		this->SetNumberOfRequiredInputs( 0 );
 
 		//Default variables values
-		m_LambdaXY = 6000;
-		m_LambdaZ  = 15000;
-		m_LambdaW  = 20000;
-		m_Iterations = 5;
-		m_GaussSeidelIterations = 200;
+
+		if(ImageDimension == 2){
+			m_LambdaXY = 6000;
+			m_LambdaZ  = 15000;
+			m_LambdaW  = 20000;
+			m_Iterations = 5;
+			m_GaussSeidelIterations = 200;
+		}
+		else
+		{
+			m_LambdaXY = 6000;
+			m_LambdaZ  = 15000;
+			m_LambdaW  = 20000;
+			m_Iterations = 5;
+			m_GaussSeidelIterations = 200;			
+		}
 		m_Threshold = 1.0;
 
+		//Default Interpolator
+		typename DefaultInterpolatorType::Pointer Interp = DefaultInterpolatorType::New();
+	  m_Interpolator =  static_cast<InterpolatorType*>( Interp.GetPointer() );
 	}
 
 
