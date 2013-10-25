0c12a36455ce92dd11e75eabe015a1496f38933b
isnardo <isnardo.rr@gmail.com>
2013-10-24 19:37:52 -0500

    Optical flow working at 100%

--- OpticalFlow/OpticalFlow.cxx
+++ OpticalFlow/OpticalFlow.cxx
@@ -19,7 +19,7 @@
 int main( int argc, char **argv ) {
 	//Types definition
 	const int	Dimensions = 3;
-	typedef itk::Image< short, Dimensions >				ImageType;
+	typedef itk::Image< short, Dimensions >								ImageType;
 	typedef itk::Image< float, Dimensions >    						FloatImageType;
 	typedef itk::Vector<float, Dimensions>								VectorPixelType;
 	typedef itk::Image<VectorPixelType,Dimensions>				VectorImageType;
@@ -64,14 +64,16 @@ int main( int argc, char **argv ) {
 	//Set variables to optical flow
 	opticalFlow->SetFixedImage( fixedImage->GetOutput() );
 	opticalFlow->SetMovingImage( movingImage->GetOutput() );
-	opticalFlow->SetLambdaXY( 3000 );
-	opticalFlow->SetLambdaZ( 15000 );
-	opticalFlow->SetLambdaW( 200000 );
-	opticalFlow->SetThreshold( 1.0 ); //Default 1.0
-	opticalFlow->SetIterations( 3 );  //Default 5
+/*	
+	opticalFlow->SetLambdaXY( 6000 );		//Default 2D = 300 and 3D = 6000
+	opticalFlow->SetLambdaZ( 15000 );		//Default 2D = 0 and 3D = 15000
+	opticalFlow->SetLambdaW( 200000 );	//Default 2D = 0 and 3D = 200000
+	opticalFlow->SetThreshold( 0.1 );  	//Default 1.0
+	opticalFlow->SetIterations( 10 );  	//Default 5
 	opticalFlow->SetGaussSeidelIterations( 200 ); //Default 200
-	opticalFlow->ShowIterationsOn(); //Default Off
-	
+	opticalFlow->ShowIterationsOn(); 		//Default Off
+*/
+
 	//Update optical flow
 	try
 	{
